58e64b8a7154770d508dd703f553aa0316bd2746
DaniAkash <s.daniakash@gmail.com>
2022-12-03 18:58:06 +0530

    feat: setup spotlight theme

--- daniakash.com/astro.config.mjs
+++ daniakash.com/astro.config.mjs
@@ -1,4 +1,15 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [react(), tailwind(), image()],
+});
