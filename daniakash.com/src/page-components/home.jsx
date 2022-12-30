3115bf7895af1383d0614f0f4b988aea662bb54b
DaniAkash <s.daniakash@gmail.com>
2022-12-30 15:16:48 +0530

    chore: updated favicon

--- daniakash.com/src/page-components/home.jsx
+++ daniakash.com/src/page-components/home.jsx
@@ -10,8 +10,8 @@ import {
 	LinkedInIcon,
 } from "@/components/SocialIcons";
 import image1 from "@/images/photos/image-1.jpg";
-import image2 from "@/images/photos/image-2.jpg";
-import image3 from "@/images/photos/image-3.jpg";
+import image3 from "@/images/photos/image-2.jpg";
+import image2 from "@/images/photos/image-3.jpg";
 import image4 from "@/images/photos/image-4.jpg";
 import image5 from "@/images/photos/image-5.jpg";
 import doodleblueLogo from "@/images/logos/doodleblue.gif";
