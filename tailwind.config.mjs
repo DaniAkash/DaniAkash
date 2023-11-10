515193680524a8b32323e8c15c0caff7fac5a43a
DaniAkash <s.daniakash@gmail.com>
2023-11-10 13:24:52 +0000

    chore: run prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,26 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontSize: {
-				xs: ["0.8125rem", { lineHeight: "1.5rem" }],
-				sm: ["0.875rem", { lineHeight: "1.5rem" }],
-				base: ["1rem", { lineHeight: "1.75rem" }],
-				lg: ["1.125rem", { lineHeight: "1.75rem" }],
-				xl: ["1.25rem", { lineHeight: "2rem" }],
-				"2xl": ["1.5rem", { lineHeight: "2rem" }],
-				"3xl": ["1.875rem", { lineHeight: "2.25rem" }],
-				"4xl": ["2rem", { lineHeight: "2.5rem" }],
-				"5xl": ["3rem", { lineHeight: "3.5rem" }],
-				"6xl": ["3.75rem", { lineHeight: "1" }],
-				"7xl": ["4.5rem", { lineHeight: "1" }],
-				"8xl": ["6rem", { lineHeight: "1" }],
-				"9xl": ["8rem", { lineHeight: "1" }],
-			},
-		},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontSize: {
+        xs: ["0.8125rem", { lineHeight: "1.5rem" }],
+        sm: ["0.875rem", { lineHeight: "1.5rem" }],
+        base: ["1rem", { lineHeight: "1.75rem" }],
+        lg: ["1.125rem", { lineHeight: "1.75rem" }],
+        xl: ["1.25rem", { lineHeight: "2rem" }],
+        "2xl": ["1.5rem", { lineHeight: "2rem" }],
+        "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
+        "4xl": ["2rem", { lineHeight: "2.5rem" }],
+        "5xl": ["3rem", { lineHeight: "3.5rem" }],
+        "6xl": ["3.75rem", { lineHeight: "1" }],
+        "7xl": ["4.5rem", { lineHeight: "1" }],
+        "8xl": ["6rem", { lineHeight: "1" }],
+        "9xl": ["8rem", { lineHeight: "1" }],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
