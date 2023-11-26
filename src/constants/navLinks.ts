0b5aefeffb76bc43f8e7e1815d75265ffd42c6f9
DaniAkash <s.daniakash@gmail.com>
2023-11-26 21:08:35 +0000

    feat: setup layout animation with nav links

---
+++ src/constants/navLinks.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Projects",
+    href: "/projects",
+  },
+  {
+    title: "Speaking",
+    href: "/speaking",
+  },
+  {
+    title: "Uses",
+    href: "/uses",
+  },
+];
