5e874cc0e54bdf634fa6675b8d7fdc12c1e19fc0
DaniAkash <s.daniakash@gmail.com>
2024-10-26 10:22:30 +0530

    feat: setup transition names

---
+++ src/constants/transition-names.tsx
@@ -0,0 +1,7 @@
+export const TRANSITION_NAMES = {
+  heroTitle: "hero-title",
+  avatar: "avatar",
+  heroImage: "hero-image-2",
+  activeNavHighlight: "active-nav-highlight",
+  responsiveNavButton: "responsive-nav-button",
+};
