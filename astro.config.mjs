8bea7b2e2beadddfd0c755071dad173359d89f51
DaniAkash <s.daniakash@gmail.com>
2024-10-28 20:00:36 +0530

    fix: broken link

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,21 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+          rel: ["noopener", "noreferrer"],
+        },
+      ],
+    ],
+  },
   experimental: {
     contentLayer: true,
     contentIntellisense: true,
