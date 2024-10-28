5943ffedf674678ab41cf3cfbcbebb4820d94329
DaniAkash <s.daniakash@gmail.com>
2024-10-28 21:55:07 +0530

    chore: remove old website code

--- daniakash.com/src/constants/links.ts
+++
@@ -1,5 +0,0 @@
-export const TWITTER = "https://twitter.com/dani_akash_";
-export const INSTAGRAM = "https://www.instagram.com/dani_akash_";
-export const GITHUB = "https://github.com/daniakash";
-export const LINKEDIN = "https://www.linkedin.com/in/daniakash/";
-export const MAIL = "mailto:s.daniakash@gmail.com";
